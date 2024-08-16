c2b4f2c5a817b49c9a6cce7fad95557715009c8f
StoppedwummSites <150438484+StoppedwummSites@users.noreply.github.com>
2024-08-16 10:51:46 +0200

    try fixing again

---
+++ __main__.py
@@ -0,0 +1,17 @@
+import sys
+from everything import *
+if __name__ == "__main__":
+    from fernet import Fernet
+    #import configreader
+    #cfg = configreader.readConfig()
+    #crypt = Fernet(cfg["encKey"].encode())
+    pwd = sys.argv[1]
+    username = sys.argv[2]
+    frontend = sys.argv[3]
+    backend = sys.argv[4]
+    webdav = sys.argv[5]
+    
+
+    s = magisSession(frontend, backend, webdav)
+    s.login(username, pwd)
+    s.logOut()
